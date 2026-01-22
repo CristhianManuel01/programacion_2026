5af030ed168084cd8069cb11d4f0d1d654092fcb
Cristian Manuel Baeza Lara <cristhianmanuelbaezalara@gmail.com>
2026-01-22 08:22:16 -0600

    Add basic Hello World program in C

---
+++ holamundo.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() // funcion principal del codigo
+{
+    printf("Hola mundo\n"); // imprime texto y imprimir salto de linea
+    return 0;               // Ejecucion exitosa
+}
