b64a3240d26b3f5a7059797589353ea6f98c32cf
Cristian Manuel Baeza Lara <cristhianmanuelbaezalara@gmail.com>
2026-01-30 12:46:28 -0600

    Add multiple student instances and update logic

--- ejercicio_01/main_estudiante.cpp
+++ ejercicio_01/main_estudiante.cpp
@@ -43,5 +43,54 @@ int main()
     estudiante2.mostrarPromedio();
     std::cout << std::endl;
 
+    Estudiante estudiante3("Emilio Sanchez Gutierres", "EST-2024-003", 24, 8.0, true);
+    std::cout << "Estudiante 3 (constructor con parametros): " << std::endl;
+    estudiante3.mostrarInformacionCompleta();
+    estudiante3.calcularEstadoAcademico();
+    std::cout << std::endl;
+
+    std::cout << "Incrementado edad de estudiande 3:" << std::endl;
+    estudiante3.incrementarEdad();
+    estudiante3.mostrarEdad();
+    std::cout << std::endl;
+
+    std::cout << "intentado actualizar promedio con valor invalido" << std::endl;
+    estudiante3.actualizarPromedio(16.0);
+    estudiante3.calcularEstadoAcademico();
+    std::cout << std::endl;
+
+    Estudiante estudiante4("george ku garcia", "EST-2024-004", 19, 6.5, true);
+    std::cout << "Estudiante 4 (constructor con parametros): " << std::endl;
+    estudiante4.mostrarInformacionCompleta();
+    estudiante4.calcularEstadoAcademico();
+    std::cout << std::endl;
+
+    std::cout << "Incrementado edad de estudiande 4:" << std::endl;
+    estudiante4.incrementarEdad();
+    estudiante4.mostrarEdad();
+    std::cout << std::endl;
+
+    std::cout << "intentado actualizar promedio con valor invalido" << std::endl;
+    estudiante4.actualizarPromedio(13.0);
+    estudiante4.calcularEstadoAcademico();
+    std::cout << std::endl;
+
+    Estudiante estudiante5("juana castillo valles", "EST-2024-005", 20, 9.5, true);
+    std::cout << "Estudiante 5 (constructor con parametros): " << std::endl;
+    estudiante5.mostrarInformacionCompleta();
+    estudiante5.calcularEstadoAcademico();
+    std::cout << std::endl;
+
+    std::cout << "Incrementado edad de estudiande 5:" << std::endl;
+    estudiante5.incrementarEdad();
+    estudiante5.mostrarEdad();
+    std::cout << std::endl;
+
+    std::cout << "intentado actualizar promedio con valor invalido" << std::endl;
+    estudiante5.actualizarPromedio(19.0);
+    estudiante5.calcularEstadoAcademico();
+    std::cout << std::endl;
+
+
     return 0;
 }
