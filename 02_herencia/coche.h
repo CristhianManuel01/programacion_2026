90ce675f1b2e05efdfeab4c42df19c663011abcd
Cristian Manuel Baeza Lara <cristhianmanuelbaezalara@gmail.com>
2026-02-13 13:14:44 -0600

    Add Coche class inheriting from Vehiculo

--- 02_herencia/coche.h
+++ 02_herencia/coche.h
@@ -1,1 +1,21 @@
+#ifndef COCHE_H
+#define COCHE_H
 
+#include "vehiculo.h"
+
+class Coche : public Vehiculo
+{
+private:
+    int numeroPuertas;
+
+public:
+    // Constructores
+    Coche();
+    Coche(string marca, string modelo, int anio, int numeroPuertas);
+
+    // Métodos
+    void mostrarInformacion(); // Sobrescritura
+    int obtenerNumeroPuertas();
+};
+
+#endif
