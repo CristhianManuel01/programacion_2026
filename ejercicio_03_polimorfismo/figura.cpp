d6ff59495898ad4d2791163d231f27f09f9381b7
Cristian Manuel Baeza Lara <cristhianmanuelbaezalara@gmail.com>
2026-03-12 10:59:24 -0600

    Implement Figura class with constructor and methods

---
+++ ejercicio_03_polimorfismo/figura.cpp
@@ -0,0 +1,15 @@
+#include "figura.h"
+
+Figura::Figura(std::string nombre)
+{
+    this->nombre = nombre;
+}
+
+Figura::~Figura()
+{
+}
+
+std::string Figura::obtenerNombre()
+{
+    return nombre;
+}
