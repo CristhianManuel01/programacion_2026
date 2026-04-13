96bf98eaf2e6e0fd7e3bd08bc94041f594e05353
Cristian Manuel Baeza Lara <cristhianmanuelbaezalara@gmail.com>
2026-04-13 08:16:48 -0600

    Implement full-time employee class with methods

---
+++ ejercicio_02_herencia/empleado_tiempo_completo.cpp
@@ -0,0 +1,56 @@
+#include "empleado_tiempo_completo.h"
+
+// Constructor por defecto
+EmpleadoTiempoCompleto::EmpleadoTiempoCompleto()
+    : Empleado()
+{
+    horasSemanales = 0;
+    bono = 0.0;
+}
+
+// Constructor con parámetros
+EmpleadoTiempoCompleto::EmpleadoTiempoCompleto(std::string nombre, std::string id,
+                                               float salarioBase,
+                                               int horasSemanales, float bono)
+    : Empleado(nombre, id, salarioBase)
+{
+    this->horasSemanales = horasSemanales;
+    this->bono = bono;
+}
+
+// Setters
+void EmpleadoTiempoCompleto::actualizarHorasSemanales(int horas)
+{
+    horasSemanales = horas;
+}
+
+void EmpleadoTiempoCompleto::actualizarBono(float bono)
+{
+    this->bono = bono;
+}
+
+// Getters
+int EmpleadoTiempoCompleto::obtenerHorasSemanales() const
+{
+    return horasSemanales;
+}
+
+float EmpleadoTiempoCompleto::obtenerBono() const
+{
+    return bono;
+}
+
+// Mostrar información (sobrescritura)
+void EmpleadoTiempoCompleto::mostrarInformacion() const
+{
+    Empleado::mostrarInformacion();
+
+    std::cout << "Horas semanales: " << horasSemanales << std::endl;
+    std::cout << "Bono: " << bono << std::endl;
+}
+
+// Calcular salario total
+float EmpleadoTiempoCompleto::calcularSalarioTotal() const
+{
+    return salarioBase + bono;
+}
